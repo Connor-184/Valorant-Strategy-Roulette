2b226edbf7683459e510aa0770a0409148aa15ed
Connor McNally <connormcnally182@gmail.com>
2021-05-11 18:47:56 -0400

    Replaced array with txt file
    Replaced the unnecessarily large array of strings containing the "general" strategies with a txt file creating the ability to add, change, or remove strategies.

--- Valorant Strategy Roulette/src/Main.java
+++ Valorant Strategy Roulette/src/Main.java
@@ -13,9 +13,12 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -37,7 +40,7 @@ public class Main extends Application {
     int numMemes = 45;
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws FileNotFoundException {
 
         // Sets root style
         String style = "-fx-background-image: url('Valorant/Background2.jpg');-fx-background-position: center center;" +
@@ -149,7 +152,6 @@ public class Main extends Application {
             borderPanes[i].setTop(hbox);
         }
 
-
         // Event handler for hyperlink
         hyperlink.setOnAction(event -> {
             if(Desktop.isDesktopSupported())
@@ -186,7 +188,11 @@ public class Main extends Application {
             if (isMS()) {
                 label[0].setText(getSplitStrat());
             } else {
-                label[0].setText(getGenStrat());
+                try {
+                    label[0].setText(getGenStrat());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
 
         });
@@ -200,7 +206,11 @@ public class Main extends Application {
             if (isMS()) {
                 label[1].setText(getBindStrat());
             } else {
-                label[1].setText(getGenStrat());
+                try {
+                    label[1].setText(getGenStrat());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
         generateButtons[2].setOnAction(event -> {
@@ -213,7 +223,11 @@ public class Main extends Application {
             if (isMS()) {
                 label[2].setText(getAscentStrat());
             } else {
-                label[2].setText(getGenStrat());
+                try {
+                    label[2].setText(getGenStrat());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
         generateButtons[3].setOnAction(event -> {
@@ -226,7 +240,11 @@ public class Main extends Application {
             if (isMS()) {
                 label[3].setText(getHavenStrat());
             } else {
-                label[3].setText(getGenStrat());
+                try {
+                    label[3].setText(getGenStrat());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
 
         });
@@ -240,7 +258,11 @@ public class Main extends Application {
             if (isMS()) {
                 label[4].setText(getIceboxStrat());
             } else {
-                label[4].setText(getGenStrat());
+                try {
+                    label[4].setText(getGenStrat());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
         generateButtons[5].setOnAction(event -> {
@@ -250,7 +272,11 @@ public class Main extends Application {
             hbox.setAlignment(Pos.CENTER_RIGHT);
             hbox.setPadding(new Insets(0, 50, 0, 0));
             borderPanes[5].setRight(hbox);
-            label[5].setText(getGenStrat());
+            try {
+                label[5].setText(getGenStrat());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 
         });
 
@@ -265,48 +291,21 @@ public class Main extends Application {
     /**
      * Returns a random general strategy.
      */
-    private static String getGenStrat() {
-        final String[] strategies = {
-                "Each teammate must buy every ability that they possibly can. Rush towards one of the sites, and use all of your abilities as fast as you can on the site. Then quickly retreat and plant the spike at a different site.",
-                "Play the round normally, however, you cannot use any normal callouts. Preferably use callouts that don't exist on this map, callouts from other games/maps, or just very broadly describe what you are trying to callout.",
-                "Elect a teammate to be the leader, your whole team must move in a line everywhere that they travel following the leader. No one can stray away from the line.",
-                "Whoever is at the bottom of the scoreboard for your team is the only person allowed to use their mic this round. They must attempt to make callouts for all of their teammates.",
-                "Everytime you kill an enemy, you must pick up their gun and use it for your next kill. It doesn't matter what gun they had or how hard it will be to pick up, you cannot get a kill with the exact same gun more than once.",
-                "Everytime a teammate dies, they must constantly blow into their mic simulating a wind sound. They cannot stop until the round is over.",
-                "Everyone on your team must set their game's master volume to 0.",
-                "Every player that is alive must narrate every action that they make. Do this as a shoutcaster would while spectating a professional match. Everyone that is alive must participate but stop if they die.",
-                "Everytime you encounter an enemy, you must do a 360 before you are allowed to shoot them.",
-                "You must jump every time you turn or peak a corner.",
-                "Everyone on your team must play the entire round crouched, you can not stand up at any time for any reason.",
-                "Your team can only buy suppressed weapons and no one on the team is allowed to speak for the entire round.",
-                "If there is an enemy on the other team with the same agent as you, you must ignore all other enemies until you have killed your doppelgänger.",
-                "Elect one player on your team to give a motivational speech at the start of the round. No one is allowed to leave spawn until the speech is finished.",
-                "You must hold the W key for the entire round and you are not allowed to walk.",
-                "No one on your team is allowed to use any abilities for this round. You might as well just be playing CS:GO.",
-                "Elect one teammate to be the 'President' who must carry the spike. The President is not allowed to have any guns or abilities, just armor. Your team must surround the president and protect them for the entire round.",
-                "The first player to die must narrate the actions of all remaining teammates.",
-                "Players who get a kill must switch to their knife for the remainder of the round.",
-                "Top frag may only use a pistol. Bottom frag may only use a sniper rifle. No two players may carry a primary weapon from the same category.",
-                "After every kill, players must stop to clap and cheer for 5 seconds in honor of the fine kill.",
-                "Choose one player to be the only person allowed to buy abilities for this round.",
-                "Everyone except for one player must use only the Sheriff for the round. Choose wisely.",
-                "Choose a player, nobody is allowed to shoot until that player gets 2 kills",
-                "Choose a player to narrate their every move until killed or the round is finished.",
-                "The top-fragger must swap loadouts with the bottom-fragger",
-                "Choose a player, they must buy and use all of their abilities (including ults if available) before anyone on the team can shoot",
-                "The player with the least amount of kills must sing the alphabet twice. Nobody is allowed to leave spawn until they finish.",
-                "The first to die will give the rest of the team instructions that must be followed for the rest of the round.",
-                "The top fragger must hold/push a site on their own while everyone else holds/pushes the opposite site.",
-                "Choose a player, they are the only person allowed to buy armor/abilities for this round.",
-                "Everyone on the team must play the round with their face 6 inches from their monitors",
-                "You can only use one hand at a time, you cannot move the mouse while pressing any keys and vice versa.",
-                "Nobody on the team is allowed to stop sprinting. No walking or crouching",
-                "After each kill you must praise a different country for that kill. Example: \"This was for MEXICO!!\"",
-                "Choose one player. Players may only move when this player is singing",
-                "Only players who are dead may use comms, nobody else is allowed to speak",
-                "Everyone must set their sensitivity to max"
+    private static String getGenStrat() throws FileNotFoundException {
+        int counter = 0;
+        File file = new File("GenStrats.txt");
+        Scanner scan = new Scanner(file);
+        Scanner scan2 = new Scanner(file);
 
-        };
+        while (scan2.hasNextLine()) {
+            scan2.nextLine();
+            counter++;
+        }
+
+        String[] strategies = new String[counter - 1];
+        for (int i = 0; i < strategies.length; i++) {
+               strategies[i] = scan.nextLine();
+           }
 
         int num = ThreadLocalRandom.current().nextInt(0, strategies.length + 1);
 
